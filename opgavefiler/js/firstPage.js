6f0d343a7edcb96c38b0081fec919ea25f763b84
simo994q <113165343+simo994q@users.noreply.github.com>
2023-03-03 09:51:00 +0100

    Initial commit

---
+++ opgavefiler/js/firstPage.js
@@ -0,0 +1,24 @@
+/* Opgave 1*/
+// din kode her
+
+const userURI = "https://jsonplaceholder.typicode.com/users";
+const myUserlistElement = document.getElementById("userList");
+
+
+/* Opgave 2*/
+
+// din kode her
+
+/* Opgave 3 - skriv videre på koden her: */
+const myDataFileUrl = "../../opgavefiler/data/story.json";
+const myStoryElement = document.getElementById("theStory");
+
+
+/* Opgave 3*/
+// din kode her
+
+const myListElement = document.getElementById("userSelect");
+const myPostElement = document.getElementById("userPosts");
+
+//entry point
+getUsers("https://jsonplaceholder.typicode.com/users");
