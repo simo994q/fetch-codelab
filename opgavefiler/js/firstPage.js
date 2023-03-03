0c5757c9841b4ff38bef28afcfe59a164e98cbef
simo994q <simonkn1@hotmail.com>
2023-03-03 13:15:21 +0100

    main

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -2,16 +2,51 @@
 // din kode her
 
 const userURI = "https://jsonplaceholder.typicode.com/users";
+
+fetch(userURI)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        buildUserList(data)
+    })
+    .catch(error => console.error(error));
+
 const myUserlistElement = document.getElementById("userList");
 
+function buildUserList(myData) {
+    myData.map((user) => {
+        myUserlistElement.innerHTML += `<h2>${user.name}</h2>
+        <p>Adresse: ${user.address.street}  ${user.address.suite}</br>
+           post nummer: ${user.address.zipcode}</br>
+           by: ${user.address.city}</p>`
+    })
+};
 
 /* Opgave 2*/
 
 // din kode her
 
-/* Opgave 3 - skriv videre på koden her: */
+
+
+
+
 const myDataFileUrl = "../../opgavefiler/data/story.json";
 const myStoryElement = document.getElementById("theStory");
+
+fetch(myDataFileUrl)
+    .then(response => response.json())
+    .then(data => {
+
+    })
+
+
+
+
+
+
+
+/* Opgave 3 - skriv videre på koden her: */
+
 
 
 /* Opgave 3*/
@@ -21,4 +56,6 @@ const myListElement = document.getElementById("userSelect");
 const myPostElement = document.getElementById("userPosts");
 
 //entry point
+
 getUsers("https://jsonplaceholder.typicode.com/users");
+
